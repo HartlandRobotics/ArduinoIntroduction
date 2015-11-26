1e64b134a1bfd8f08d994a41795bbbd1809ce78d
Brian Genisio <BrianGenisio@Gmail.com>
2015-11-25 20:44:09 -0500

    updating the strobe.js example

--- examples/strobe.js
+++ examples/strobe.js
@@ -1,8 +1,15 @@
 var five = require("johnny-five");
-var board = new five.Board();
+var Particle = require("particle-io");
+
+var board = new five.Board({
+  io: new Particle({
+    token: process.env.PARTICLE_TOKEN,
+    deviceId: process.env.PARTICLE_DEVICE_ID
+  })
+});
 
 board.on("ready", function() {
 
-  (new five.Led(11)).strobe();
+  (new five.Led("D7")).strobe();
 
 });
